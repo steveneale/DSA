edaf1bd483db4ed1d677fbbd4de87e5cbe206333
Steve Neale <steveneale3000@gmail.com>
2019-11-16 12:56:14 +0000

    Split sorting code into .hpp and .cpp
    Split the code from sorting.hpp so that the function is defined in
    sorting.hpp, and the function logic is implemented in sorting.cpp.

---
+++ src/arrays/sorting.cpp
@@ -0,0 +1,19 @@
+#include <DSA++/arrays/sorting.hpp>
+
+namespace arrays
+{
+    namespace sorting
+    {
+        void insertion_sort(int array[], int array_size) {
+            for (int i = 0; i < array_size; i++) {
+                int key = array[i];
+                int j = i-1;
+                while (j >= 0 && array[j] > key) {
+                    array[j+1] = array[j];
+                    j -= 1;
+                    array[j+1] = key;
+                }
+            }
+        }
+    }
+}
